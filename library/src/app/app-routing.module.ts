7e8ebe5141484d2f166e5481b4c03b5c2225dff5
Eddie Vallier <accounts@eddievallier.fr>
2022-04-13 10:47:59 +0200

    add auth component

--- library/src/app/app-routing.module.ts
+++ library/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DisplayBooksComponent} from "./components/display-books/display-books.component";
 import {DisplayAuthorsComponent} from "./components/display-authors/display-authors.component";
+import {AuthComponent} from "./components/auth/auth.component";
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'displayAuthors',
     component: DisplayAuthorsComponent
+  },
+  {
+    path: 'auth',
+    component: AuthComponent
   }
 ];
 
